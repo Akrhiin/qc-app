410bdd9b135a64a139e8a672c53e7fca554dab57
Akrhiin <michaelpace.ma@protonmail.com>
2025-03-28 01:45:33 -0400

    0.2.0 - basic landing page implemented

---
+++ qc-app/src/index/index.jsx
@@ -0,0 +1,24 @@
+import './index.css'
+
+function Index() {
+    return (
+        <>
+        <main>
+            <a className="box" id="find-class">
+                <h2>Find my Class</h2>
+                <p>Search for rooms across QC campuses</p>
+            </a>
+            <a className="box" id="quick-links">
+                <h2>Quick Links</h2>
+                <p>Access frequently used resources</p>
+            </a>
+            <a className="box" id="schedule">
+                <h2>My Schedule</h2>
+                <p>View important dates and deadlines</p>
+            </a>
+        </main>
+        </>
+    )
+}
+
+export default Index
--- qc-app/src/main.jsx
+++ qc-app/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './main.css'
+import Index from './index/index.jsx'
+
+let pages = ["Index","Room","Links","Learn"];
+function updatePage(new_page) {
+    switch (new_page) {
+        case 0: // index
+            break;
+        case 1: // room finder
+            break;
+        case 2: // quick links
+            break;
+        case 3: // learning assistance/tools
+            break;
+        default:
+            // write code to yell a bunch
+    }
+}
+
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <Index />
     </StrictMode>
 )
